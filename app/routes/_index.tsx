92b14e7e8d82ea031243d4a9472387f7ab4fa75e
RC Maples <rc@rcmaples.io>
2024-06-03 14:08:38 -0400

    finished tut

---
+++ app/routes/_index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <p id="index-page">
+      This is a demo for remix. <br />
+      Check out <a href="https://remix.run">the docs at remmix.run</a>
+    </p>
+  );
+}
--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,17 +1,31 @@
-import type { LoaderFunctionArgs } from '@remix-run/node';
-import { json } from '@remix-run/node';
-import { Form, useLoaderData } from '@remix-run/react';
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from '@remix-run/node';
+import { Form, useFetcher, useLoaderData } from '@remix-run/react';
 import type { FunctionComponent } from 'react';
+import invariant from 'tiny-invariant';
 
-import { getContact } from '../data';
+import { getContact, updateContact, ContactRecord } from '../data';
 
-export const loader = async ({ params }) => {
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  invariant(params.contactId, 'missing contractId param');
+  const formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get('favorite') === 'true',
+  });
+};
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, 'Missing contactId param');
   const contact = await getContact(params.contactId);
+
+  if (!contact) {
+    throw new Response('not found', { status: 404 });
+  }
+
   return json({ contact });
 };
 
 export default function Contact() {
-  const contact = useLoaderData<typeof loader>();
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
@@ -72,16 +86,19 @@ export default function Contact() {
 const Favorite: FunctionComponent<{
   contact: Pick<ContactRecord, 'favorite'>;
 }> = ({ contact }) => {
-  const favorite = contact.favorite;
+  const fetcher = useFetcher();
+  const favorite = fetcher.formData
+    ? fetcher.formData.get('favorite') === 'true'
+    : contact.favorite;
 
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
         name="favorite"
         value={favorite ? 'false' : 'true'}>
         {favorite ? '★' : '☆'}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 };
